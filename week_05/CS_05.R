e446f10dc9d5c65beee4ad341f190b628a5c2d92
Enoch Yeleliere <enochyel@buffalo.edu>
2024-10-04 18:45:25 -0400

    Case study 4

--- week_05/CS_05.R
+++ week_05/CS_05.R
@@ -1,51 +1,45 @@
 #Install packages
-install.packages("spData")
 install.packages("sf")
+install.packages("spData")
 install.packages("tidyverse")
 
-#Load data and packages for task 5
-library (spData)   #data for case 5
-library(dplyr)     #For data manipulation
-library(sf)        #For handling spatial data
-library(ggplot2)   #For plotting
 
-#Data===============Load data from the spData package
-data(world)
-# load 'states' boundaries from spData package
-data(us_states)
-# plot(world[1])          #plot if desired
-# plot(us_states[1])      #plot if desired
-# plot "world" using gglot2
+#Load packages in libraries for task 5
+library(sf)
+library(spData)
+library(tidyverse)
+
+
+#Data===============Load data
+data("world")   #load 'world data from spData package
+
+data("us_states")       #Load 'states' boundaries from the spData
+
+plot(world[1])          #plot if desired
+
+plot (us_states[1])     #plot if desired
+
 
 #Working with world dataset
 # Transform to albers equal area projections
-albers="+proj=aea +lat_1=29.5 +lat_2=45.5 
-+lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 
-+datum=NAD83 +units=m +no_defs"
+albers="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 
++x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 
-## Data Processing
-Canada_only <- world %>% filter(name_long== "Canada") %>% select(geom)
-Newyork_only <- us_states %>% filter(NAME== "New York") %>% select(geometry)
+#Data Processing
 
-# Define Projection
-albers="+proj=aea +lat_1=29.5 +lat_2=45.5 
-+lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 
-+ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+canada_only<- world %>%
+  filter(name_long == "Canada") %>%
+  select(geom)       #filter Canada from the world data 
+NewYork_only<- world %>%
+  filter(name == "New York") %>%
+  select(geom)      #filter New York State
+world
+#Create a border object
+albers="+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 
++lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m 
++no_defs"
 
-#Transform data
-Canada_only <- st_transform(Canada_only, albers)
-Newyork_only <- st_transform(Newyork_only, albers)
-
-# Create a 10 km (10,000 meters) buffer around Canada
-Canada_buffered <- st_buffer(Canada_only, dist = 10000)  # 10 km = 10,000 meters
-
-# Use st_intersection() to intersect the Canada buffer with New York state
-border <- st_intersection(Canada_buffered, Newyork_only)
-
-# Visualizing the data
-ggplot() +
-  geom_sf(data = Newyork_only) +
-  geom_sf(data = border, colour = "red", fill = "red") +
-  ggtitle("New York Land within 10km")
+canada_only <- st_transform(canada_only, albers)
+NewYork_only <- st_transform(NewYork_only, albers)
 
 
